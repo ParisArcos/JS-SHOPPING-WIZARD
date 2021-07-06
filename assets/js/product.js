cc3d0d26bb8a927ab49ba35b43793fd868bb84f9
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-07-06 16:59:18 +0100

    index->Slides auto-generator

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,25 +1,36 @@
-//image carousel
-for (const i of minis) {
-  i.addEventListener("click", bigSlide);
-}
-
-function bigSlide(event) {
-  var event = event.target;
-  for (const iterator of minis) {
-    iterator.classList.remove("active");
-  }
-  event.classList.add("active");
-  slide.src = event.src;
-}
-
 //variable value (in)
 document.getElementById("name").innerHTML = product.name;
 document.getElementById("price").innerHTML = product.price + "€";
+document.getElementById("productName").value = product.name;
+document.getElementById("productPrice").value = product.price;
+//tallas
 for (let x = 0; x < product.size.length; x++) {
   var size = document.createElement("option");
   size.text = product.size[x];
   size.value = product.size[x];
   select1.appendChild(size);
+}
+//colores
+
+for (let y = 0; y < product.combination.length; y++) {
+  var img = document.createElement("img");
+  img.setAttribute("class", y == 0 ? "micro cursor selected" : "micro cursor");
+  img.setAttribute("data-Hex", product.combination[y].colorHex);
+  img.src = product.combination[y].imageURL;
+  img.alt = product.combination[y].colorName;
+  select2.appendChild(img);
+}
+
+for (const i of micros) {
+  i.addEventListener("click", colorSlide);
+}
+
+function colorSlide(event) {
+  var event = event.target;
+  for (const iterator of micros) {
+    iterator.classList.remove("selected");
+  }
+  event.classList.add("selected");
 }
 
 //variable value (out)
@@ -35,7 +46,47 @@ function getValues(event) {
 select1.addEventListener("submit", function () {
   product.colors.color = select1.value;
 });
-
+//cmabiar despues de img
 select2.addEventListener("submit", function () {
   product.size = select2.value;
 });
+
+//images colour
+
+product.combination.forEach((combination) => {
+  if (combination.colorHex === selected[0].dataset.hex) {
+    //<img id="img-big" class="img-fluid" src="assets/img/600x400-1.png" alt="Main-Image" />
+    var imgB = document.createElement("img");
+    imgB.setAttribute("class", "img-fluid");
+    imgB.src = combination.imageURL;
+    imgB.alt = combination.colorName;
+    imgB.id = "img-big";
+    mainSlide.appendChild(imgB);
+    for (let i = 0; i < combination.secondaryImg.length; i++) {
+      //<img class="mini cursor active" src="assets/img/600x400-1.png" alt="Small-Image1" /><!--img1-->
+      var imgSmll = document.createElement("img");
+      imgSmll.setAttribute(
+        "class",
+        i == 0 ? "mini cursor active" : "mini cursor"
+      );
+      imgSmll.src = combination.secondaryImg[i];
+      imgSmll.alt = combination.colorName;
+      imgSmll.id = "mini";
+      carrousel.appendChild(imgSmll);
+    }
+  }
+});
+console.log(mainSlide.childNodes[1].src);
+//images carousel
+for (const i of minis) {
+  i.addEventListener("click", bigSlide);
+}
+
+function bigSlide(event) {
+  var event = event.target;
+  for (const iterator of minis) {
+    iterator.classList.remove("active");
+  }
+  event.classList.add("active");
+  mainSlide.childNodes[1].src = event.src;
+}
--- assets/js/variables.js
+++ assets/js/variables.js
@@ -14,7 +14,7 @@ var user = {
   country: "",
   phoneCountry: "",
   phoneNumber: "",
-  regularAddress: false
+  regularAddress: false,
 };
 /**
  * Product Global Variables
@@ -22,84 +22,84 @@ var user = {
 var product = {
   name: "Camiseta básica",
   price: 9.99,
-  size: ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
+  size: ["XS", "S", "M", "L", "XL", "XXL"],
   combination: [
     {
-      colorName: 'Arena',
-      imageURL: 'assets/img/products/arena/0.webp',
-      colorHex: '#bbac9a',
+      colorName: "Arena",
+      imageURL: "assets/img/products/arena/0.webp",
+      colorHex: "#bbac9a",
       secondaryImg: [
-          'assets/img/products/arena/1.webp',
-          'assets/img/products/arena/2.webp',
-          'assets/img/products/arena/3.webp',
-          'assets/img/products/arena/4.webp'
-      ]
+        "assets/img/products/arena/1.webp",
+        "assets/img/products/arena/2.webp",
+        "assets/img/products/arena/3.webp",
+        "assets/img/products/arena/4.webp",
+      ],
     },
     {
-      colorName: 'Blanco',
-      imageURL: 'assets/img/products/blanco/0.webp',
-      colorHex: '#fff',
+      colorName: "Blanco",
+      imageURL: "assets/img/products/blanco/0.webp",
+      colorHex: "#fff",
       secondaryImg: [
-          'assets/img/products/blanco/1.webp',
-          'assets/img/products/blanco/2.webp',
-          'assets/img/products/blanco/3.webp',
-          'assets/img/products/blanco/4.webp'
-      ]
+        "assets/img/products/blanco/1.webp",
+        "assets/img/products/blanco/2.webp",
+        "assets/img/products/blanco/3.webp",
+        "assets/img/products/blanco/4.webp",
+      ],
     },
     {
-      colorName: 'Gris medio',
-      imageURL: 'assets/img/products/gris-medio/0.webp',
-      colorHex: '#646174',
+      colorName: "Gris medio",
+      imageURL: "assets/img/products/gris-medio/0.webp",
+      colorHex: "#646174",
       secondaryImg: [
-          'assets/img/products/gris-medio/1.webp',
-          'assets/img/products/gris-medio/2.webp',
-          'assets/img/products/gris-medio/3.webp',
-          'assets/img/products/gris-medio/4.webp'
-      ]
+        "assets/img/products/gris-medio/1.webp",
+        "assets/img/products/gris-medio/2.webp",
+        "assets/img/products/gris-medio/3.webp",
+        "assets/img/products/gris-medio/4.webp",
+      ],
     },
     {
-      colorName: 'Kaki',
-      imageURL: 'assets/img/products/kaki/0.webp',
-      colorHex: '#c6d47d',
+      colorName: "Kaki",
+      imageURL: "assets/img/products/kaki/0.webp",
+      colorHex: "#c6d47d",
       secondaryImg: [
-          'assets/img/products/kaki/1.webp',
-          'assets/img/products/kaki/2.webp',
-          'assets/img/products/kaki/3.webp',
-          'assets/img/products/kaki/4.webp'
-      ]
+        "assets/img/products/kaki/1.webp",
+        "assets/img/products/kaki/2.webp",
+        "assets/img/products/kaki/3.webp",
+        "assets/img/products/kaki/4.webp",
+      ],
     },
     {
-      colorName: 'Lila',
-      imageURL: 'assets/img/products/lila/0.webp',
-      colorHex: '#c6bbd5',
+      colorName: "Lila",
+      imageURL: "assets/img/products/lila/0.webp",
+      colorHex: "#c6bbd5",
       secondaryImg: [
-          'assets/img/products/lila/1.webp',
-          'assets/img/products/lila/2.webp',
-          'assets/img/products/lila/3.webp',
-          'assets/img/products/lila/4.webp'
-      ]
+        "assets/img/products/lila/1.webp",
+        "assets/img/products/lila/2.webp",
+        "assets/img/products/lila/3.webp",
+        "assets/img/products/lila/4.webp",
+      ],
     },
     {
-      colorName: 'Lima',
-      imageURL: 'assets/img/products/lima/0.webp',
-      colorHex: '#646174',
+      colorName: "Lima",
+      imageURL: "assets/img/products/lima/0.webp",
+      colorHex: "#646174",
       secondaryImg: [
-          'assets/img/products/lima/1.webp',
-          'assets/img/products/lima/2.webp',
-          'assets/img/products/lima/3.webp',
-          'assets/img/products/lima/4.webp'
-      ]
+        "assets/img/products/lima/1.webp",
+        "assets/img/products/lima/2.webp",
+        "assets/img/products/lima/3.webp",
+        "assets/img/products/lima/4.webp",
+      ],
     },
     {
-      colorName: 'Negro',
-      imageURL: 'assets/img/products/negro/0.webp',
-      colorHex: '#131217',
+      colorName: "Negro",
+      imageURL: "assets/img/products/negro/0.webp",
+      colorHex: "#131217",
       secondaryImg: [
-          'assets/img/products/negro/1.webp',
-          'assets/img/products/negro/2.webp',
-          'assets/img/products/negro/3.webp',
-          'assets/img/products/negro/4.webp'
-      ]
+        "assets/img/products/negro/1.webp",
+        "assets/img/products/negro/2.webp",
+        "assets/img/products/negro/3.webp",
+        "assets/img/products/negro/4.webp",
+      ],
     },
   ],
 };
@@ -122,8 +122,14 @@ var gift = {
 /**
  * Index Global Variables
  */
-
+var mainSlide = document.getElementById("mainSlide");
+var carrousel = document.getElementById("carrousel");
 var slide = document.getElementById("img-big");
-var minis = document.querySelectorAll(".mini");
+var minis = document.getElementsByClassName("mini");
+var micros = document.getElementsByClassName("micro");
 var count = 0;
+var productForm = document.getElementById("product-form");
+var select1 = document.getElementById("selection1");
+var select2 = document.getElementById("selection2");
+var selected = document.getElementsByClassName("selected");
 //onclick event-> forOf-> currentSlide(index)
