66ff775e78049f27efd18d9e56ac36224c57d701
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-07-06 12:31:22 +0100

    index-> name,price & size values (IN)

--- assets/js/product.js
+++ assets/js/product.js
@@ -11,7 +11,16 @@ function bigSlide(event) {
   event.classList.add("active");
   slide.src = event.src;
 }
+
 //variable value (in)
+document.getElementById("name").innerHTML = product.name;
+document.getElementById("price").innerHTML = product.price + "€";
+for (let x = 0; x < product.size.length; x++) {
+  var size = document.createElement("option");
+  size.text = product.size[x];
+  size.value = product.size[x];
+  select1.appendChild(size);
+}
 
 //variable value (out)
 productForm.addEventListener("submit", getValues);
@@ -20,16 +29,13 @@ function getValues(event) {
   for (const value of formValues.entries()) {
     value[0] == "productName" ? (product.name = value[1]) : "";
     value[0] == "productPrice" ? (product.price = value[1]) : "";
-    console.log(value[1]);
   }
 }
 
 select1.addEventListener("submit", function () {
   product.colors.color = select1.value;
-  console.log(select1.value);
 });
 
 select2.addEventListener("submit", function () {
   product.size = select2.value;
-  console.log(select2.value);
 });
