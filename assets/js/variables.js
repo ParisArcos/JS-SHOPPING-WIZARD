38119ebf9feb887239d800aa80f74533695f323f
Cherrerotinoco <c.herrerotinoco@gmail.com>
2021-07-06 12:21:29 +0200

    Adding more product images

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -22,41 +22,83 @@ var product = {
   name: 'Camiseta básica',
   price: 9.99,
   size: ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
-  colors: [
+  combination: [
     {
       colorName: 'Arena',
-      imageURL: 'assets/img/arena.webp',
-      colorHex: '#bbac9a'
+      imageURL: 'assets/img/products/arena/0.webp',
+      colorHex: '#bbac9a',
+      secondaryImg: [
+          'assets/img/products/arena/1.webp',
+          'assets/img/products/arena/2.webp',
+          'assets/img/products/arena/3.webp',
+          'assets/img/products/arena/4.webp'
+      ]
     },
     {
       colorName: 'Blanco',
-      imageURL: 'assets/img/arena.webp',
-      colorHex: '#fff'
+      imageURL: 'assets/img/products/blanco/0.webp',
+      colorHex: '#fff',
+      secondaryImg: [
+          'assets/img/products/blanco/1.webp',
+          'assets/img/products/blanco/2.webp',
+          'assets/img/products/blanco/3.webp',
+          'assets/img/products/blanco/4.webp'
+      ]
     },
     {
       colorName: 'Gris medio',
-      imageURL: 'assets/img/arena.webp',
-      colorHex: '#646174'
+      imageURL: 'assets/img/products/gris-medio/0.webp',
+      colorHex: '#646174',
+      secondaryImg: [
+          'assets/img/products/gris-medio/1.webp',
+          'assets/img/products/gris-medio/2.webp',
+          'assets/img/products/gris-medio/3.webp',
+          'assets/img/products/gris-medio/4.webp'
+      ]
     },
     {
       colorName: 'Kaki',
-      imageURL: 'assets/img/arena.webp',
-      colorHex: '#c6d47d'
+      imageURL: 'assets/img/products/kaki/0.webp',
+      colorHex: '#c6d47d',
+      secondaryImg: [
+          'assets/img/products/kaki/1.webp',
+          'assets/img/products/kaki/2.webp',
+          'assets/img/products/kaki/3.webp',
+          'assets/img/products/kaki/4.webp'
+      ]
     },
     {
       colorName: 'Lila',
-      imageURL: 'assets/img/arena.webp',
-      colorHex: '#c6bbd5'
+      imageURL: 'assets/img/products/lila/0.webp',
+      colorHex: '#c6bbd5',
+      secondaryImg: [
+          'assets/img/products/lila/1.webp',
+          'assets/img/products/lila/2.webp',
+          'assets/img/products/lila/3.webp',
+          'assets/img/products/lila/4.webp'
+      ]
     },
     {
       colorName: 'Lima',
-      imageURL: 'assets/img/arena.webp',
-      colorHex: '#646174'
+      imageURL: 'assets/img/products/lima/0.webp',
+      colorHex: '#646174',
+      secondaryImg: [
+          'assets/img/products/lima/1.webp',
+          'assets/img/products/lima/2.webp',
+          'assets/img/products/lima/3.webp',
+          'assets/img/products/lima/4.webp'
+      ]
     },
     {
       colorName: 'Negro',
-      imageURL: 'assets/img/arena.webp',
-      colorHex: '#131217'
+      imageURL: 'assets/img/products/negro/0.webp',
+      colorHex: '#131217',
+      secondaryImg: [
+          'assets/img/products/negro/1.webp',
+          'assets/img/products/negro/2.webp',
+          'assets/img/products/negro/3.webp',
+          'assets/img/products/negro/4.webp'
+      ]
     },
   ]
 };
@@ -81,4 +123,3 @@ var slide = document.getElementById("img-big");
 var minis = document.querySelectorAll(".mini");
 var count = 0;
 //onclick event-> forOf-> currentSlide(index)
-//
